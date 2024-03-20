9bb8ab508ad3ec4c8cfccf5d1976b070a0f12b10
ServerGCP <163532456+ServerGCP@users.noreply.github.com>
2024-03-20 14:54:12 +0530

    Update pmaps.py

--- pmaps.py
+++ pmaps.py
@@ -15,13 +15,13 @@ def processing(df):
     df = df[df['SectionName'] == 'PMaps Sales Orientation']
     
     st.text("Step 2: Selecting relevant columns...")
-    df = df[['CandidateUniqueId', 'OriginalQuestionId', 'OriginalOptionId']]
+    df = df[['CandidateUniqueId', 'EmailAddress', 'OriginalQuestionId', 'OriginalOptionId']]
     
     st.text("Step 3: Dropping specified question IDs...")
     df = df[~df['OriginalQuestionId'].isin(ques_id_to_drop)]
     
     st.text("Step 4: Grouping by candidate and question...")
-    df = df.groupby(['CandidateUniqueId', 'OriginalQuestionId']).first().reset_index()
+    df = df.groupby(['EmailAddress', 'OriginalQuestionId']).last().reset_index()
     
     st.text("Step 5: Pivoting the DataFrame...")
     df_pivoted = df.pivot(index='CandidateUniqueId', columns='OriginalQuestionId', values='OriginalOptionId')
@@ -40,32 +40,50 @@ def processing(df):
 def main():
     st.title("PMaps Prediction App")
     model = pickle.load(open('model.pkl', 'rb'))
-    
+
     st.sidebar.title("Upload Positonal Data")
-    uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=["csv"])
     
-    if uploaded_file is not None:
-        st.sidebar.text("Processing...")
-        df = pd.read_csv(uploaded_file)
-        processed_df = processing(df)
+    instructions = """
+    Instructions:
+    Please provide a CSV file containing the following mandatory columns:
+
+    1. `CandidateUniqueId`: Unique identifier for each candidate.
+    2. `EmailAddress`: Email address of the candidate.
+    3. `SectionName`: Name of the section in the test.
+    4. `OriginalQuestionId`: Identifier for the original question.
+    5. `OriginalOptionId`: Identifier for the original option selected by the candidate.
+
+    Ensure that the CSV file is sorted in a positional manner, with the first entry representing the results of the first attempt.
+    """
+    st.sidebar.markdown(instructions)
+    st.sidebar.markdown("Please confirm that you have read and understood the instructions below before proceeding.")
+    instructions_confirmed = st.sidebar.checkbox("I confirm that I have read and understood the instructions")
+    
+    if instructions_confirmed:
+        uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=["csv"])
         
-        st.sidebar.text("Making Predictions...")
-        preds = model.predict(processed_df)
-        probs = model.predict_proba(processed_df)
-        probs = list(map(lambda x: str(round(100*max(x),2))+'%', probs))
-
-        df_out = processed_df.copy(deep=True)
-        df_out['Performance'] = list(map(lambda x: x.split('_')[0] , preds))
-        df_out['Attrition'] = list(map(lambda x: x.split('_')[1] , preds))
-        df_out['Confidence'] = probs
-
-        df_out = df_out[['Performance', 'Attrition', 'Confidence']].astype(str)
-        st.sidebar.text("Saving Predictions...")
-        st.sidebar.text("Completed!")
-
-        st.markdown(get_download_link(df_out, 'prediction_data.csv'), unsafe_allow_html=True)
-        st.write("### Predictions")
-        st.write(df_out)
+        if uploaded_file is not None:
+            st.sidebar.text("Processing...")
+            df = pd.read_csv(uploaded_file)
+            processed_df = processing(df)
+            
+            st.sidebar.text("Making Predictions...")
+            preds = model.predict(processed_df)
+            probs = model.predict_proba(processed_df)
+            probs = list(map(lambda x: str(round(100*max(x),2))+'%', probs))
+    
+            df_out = processed_df.copy(deep=True)
+            df_out['Performance'] = list(map(lambda x: x.split('_')[0] , preds))
+            df_out['Attrition'] = list(map(lambda x: x.split('_')[1] , preds))
+            df_out['Confidence'] = probs
+    
+            df_out = df_out[['Performance', 'Attrition', 'Confidence']].astype(str)
+            st.sidebar.text("Saving Predictions...")
+            st.sidebar.text("Completed!")
+    
+            st.markdown(get_download_link(df_out, 'prediction_data.csv'), unsafe_allow_html=True)
+            st.write("### Predictions")
+            st.write(df_out)
 
 if __name__ == "__main__":
     main()
